e5150f34169fd2a2877b30000fd3c3f50c3216b9
vinicius <viniciusbarbosa@acad.ifma.edu.br>
2022-11-20 23:54:06 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import SalesCard from "./components/SalesCard"
 import Header from "./components/Header"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
         <>
+            <ToastContainer />
             <Header />
             <main>
                 <section id="sales">
