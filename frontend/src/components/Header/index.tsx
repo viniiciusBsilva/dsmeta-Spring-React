2d6ff8be7bb41b6c632973a8542d53e23c629840
vinicius <viniciusbarbosa@acad.ifma.edu.br>
2022-11-21 00:15:11 -0300

    adcionando link correto

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.linkedin.com/in/vinicius-barbosa-6b2797226/">@vinicius-barbosa</a>
                 </p>
             </div>
         </header>
